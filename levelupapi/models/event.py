c6746268232856d3b29e0e54dbf89cd7509c9929
Danielle Martin <ordinaryworld2014@gmail.com>
2022-08-02 13:40:14 -0500

    Finished models

---
+++ levelupapi/models/event.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Event(models.Model):
+
+    game = models.ForeignKey("Game", on_delete=models.CASCADE)
+    description = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    organizers = models.ForeignKey("Gamer", on_delete=models.CASCADE)
+    attendees = models.ManyToManyField(
+        "Gamer", through="event_gamer", related_name="attending")
