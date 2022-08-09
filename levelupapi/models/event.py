75fd9a9a0193a24cef64c13444bcd13d5e584222
Danielle Martin <ordinaryworld2014@gmail.com>
2022-08-08 19:43:39 -0500

    Updated

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -4,19 +4,16 @@ from django.db import models
 class Event(models.Model):
 
     game = models.ForeignKey("Game", on_delete=models.CASCADE)
-    description = models.CharField(max_length=100)
+    description = models.TextField()
     date = models.DateField()
     time = models.TimeField()
-    organizers = models.ForeignKey("Gamer", on_delete=models.CASCADE)
-    attendees = models.ManyToManyField(
-        "Gamer", through="event_gamer", related_name="attending")
+    organizer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
+    attendees = models.ManyToManyField("Gamer", related_name="events")
 
+    @property
+    def joined(self):
+        return self.__joined
 
-@property
-def joined(self):
-    return self.__joined
-
-
-@joined.setter
-def joined(self, value):
-    self.__joined = value
+    @joined.setter
+    def joined(self, value):
+        self.__joined = value
